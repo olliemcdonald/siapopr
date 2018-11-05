a55cb6e9c2e87f134d96c2b6b5e72b37d08af904
Ollie McDonald <mcdonald@jimmy.harvard.edu>
2018-11-05 12:29:08 -0500

    fixed plugin for fitness distributions

--- R/customize.R
+++ R/customize.R
@@ -109,23 +109,34 @@ create_newclone_template <- function(cppfile = "custom_newclone_plugin.cpp"){
 compile_custom_fitness <- function(cppfile){
   cpproot <- .pop_off(cppfile, ".", fixed = T)
   cppsuffix <- .pop(cppfile, ".", fixed = T)
+  Makevars.path <- .pop_off(cpproot, "/", fixed = T)
 
   concopy <- paste(cppfile, ".backup", sep = "")
   file.copy(cppfile, concopy)
   if(.Platform$OS.type == "windows")
   {
-    file.copy(paste(.libPaths()[1], "/siapopr/extras/Makevars.win", sep = ""), "Makevars.win")
-
+    Makevars.file <- paste(Makevars.path, "/Makevars.win", sep = "")
+    file.copy(paste(path.package("siapopr"), "/extras/Makevars.win", sep = ""),
+              Makevars.file,
+              overwrite = T)
+    write(paste("PKG_CXXFLAGS += -I", path.package("siapopr"), "/include", sep = ""),
+          file = Makevars.file, append = T)
   }
   else if(.Platform$OS.type == "unix")
   {
-    file.copy(paste(.libPaths()[1], "/siapopr/extras/Makevars", sep = ""), "Makevars")
+    Makevars.file <- paste(Makevars.path, "/Makevars", sep = "")
+    file.copy(paste(path.package("siapopr"), "/extras/Makevars", sep = ""),
+              Makevars.file,
+              overwrite = T)
+    path.package("siapopr")
+    write(paste("PKG_CXXFLAGS += -I", path.package("siapopr"), "/include", sep = ""),
+          file = Makevars.file, append = T)
   }
   else
   {
     stop("Customization only support on Windows, OS X, and Linux systems")
   }
-
+  Sys.setenv(PKG_CXXFLAGS=paste("-I", path.package("siapopr"), "/include", sep = ""))
   # Need to make windows version
   shlib <- paste("R CMD SHLIB", cppfile)
   system(shlib)
