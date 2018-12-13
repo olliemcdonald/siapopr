a8b972f3d81f2cf8efb9b38aa50f0db7a6fdde06
Ollie McDonald <olliemcdonald@users.noreply.github.com>
2018-12-13 08:05:57 -0500

    output diagnostic values when summand goes beyond rate

--- src/constantCloneList.cpp
+++ src/constantCloneList.cpp
@@ -355,6 +355,9 @@ void ConstantCloneList::AdvanceState(double curr_time, double next_time, gsl_rng
   if (!flag)
   {
     std::cout << "error: step not completed" << "\n";
+    std::cout << "rand_value:\t" << rand_next_event << "\n";
+    std::cout << "total rate:\t" << tot_rate << "\n";
+    std::cout << "current_summand:\t" << summand << "\n";
     exit(0);
   }
 }
